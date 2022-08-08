841ecf81ec299943f925f6ff84cadb2f9ef97182
IND-CHN-LT12191\I4921 <shriraam.ramesh@indiumsoft.com>
2022-08-08 19:48:47 +0530

    test commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.render(
 	<React.StrictMode>
